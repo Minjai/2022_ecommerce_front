027334085ef1caaf184c3cf48b90f6636753a8da
Nurs.K <nursultandev@gmail.com>
2023-01-05 17:24:54 +0600

    add ssl and chagne API URL

--- src/constants/axios.js
+++ src/constants/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_URL = 'http://127.0.0.1:8000/api/v1/'
+export const API_URL = 'http://82.221.141.66/api/v1/'
 
 export const axiosInstance = axios.create({
   baseURL: API_URL
