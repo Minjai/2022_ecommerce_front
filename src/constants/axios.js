75c3bbec0b801bdcedeb2c2e526d17350b44b5b3
Nurs.K <nursultandev@gmail.com>
2023-01-05 16:33:16 +0600

    deplay project

--- src/constants/axios.js
+++ src/constants/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_URL = 'http://167.172.39.139/api/v1/'
+export const API_URL = 'http://127.0.0.1:8000/api/v1/'
 
 export const axiosInstance = axios.create({
   baseURL: API_URL
